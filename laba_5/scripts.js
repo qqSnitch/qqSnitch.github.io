5be84b72f71bacdecbaee9da83e69318705d32e6
qqSnitch <128982937+qqSnitch@users.noreply.github.com>
2023-10-10 13:17:57 +0300

    Update scripts.js

--- laba_5/scripts.js
+++ laba_5/scripts.js
@@ -1,25 +1,24 @@
-document.addEventListener("DOMContentLoaded", function (){ 
+document.addEventListener("DOMContentLoaded", function () {
     let button1 = document.getElementById("button1");
     button1.addEventListener("click", index);
 });
-function check()
-{
+function check() {
     let a = document.getElementById("field").value;
-    if(a>0){return true;}
+    if (a > 0) {
+        return true;
+    }
 }
-function index(e) { 
+function index(e) {
     e.preventDefault();
     let select = document.getElementById("select");
     let field = document.getElementById("field");
     let result = document.getElementById("result");
     let sum = 0;
-    if(check())
-    {
+    if (check()) {
         sum = parseInt(select.options[select.selectedIndex].value);
-        sum = parseInt(field.value) *sum;
-        result.innerHTML =sum;
+        sum = parseInt(field.value) * sum;
+        result.innerHTML = sum;
         return false;
-    }else{
-        alert ("Не правильный ввод")
     }
-  }
+    window.alert("Не правильный ввод");
+}
